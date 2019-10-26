945341f1204ad1d9ad7e8396c51fbd79f1dd8fc9
bartoszkruba <bartosz.kruba@gmail.com>
2019-10-26 15:22:57 +0200

    Initial commit

---
+++ android/src/com/ktx/shooter_demo/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.ktx.shooter_demo;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.ktx.shooter_demo.Game;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Game(), config);
+	}
+}
---
+++ desktop/src/com/ktx/shooter_demo/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ktx.shooter_demo.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ktx.shooter_demo.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Game(), config);
+	}
+}
